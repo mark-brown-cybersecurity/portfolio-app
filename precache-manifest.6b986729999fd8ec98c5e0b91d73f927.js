4034fb5605a9a56754a1ff41e3fc465e650213ae
Mark <markbrowncms@gmail.com>
2019-09-29 02:41:43 -0700

    Updates

---
+++ precache-manifest.6b986729999fd8ec98c5e0b91d73f927.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d7495d865acf294596b45ea7626a580",
+    "url": "/portfolio-app/index.html"
+  },
+  {
+    "revision": "cefcb3aae32823a63630",
+    "url": "/portfolio-app/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "d31d38cc9d1353552328",
+    "url": "/portfolio-app/static/css/main.7d4724d6.chunk.css"
+  },
+  {
+    "revision": "cefcb3aae32823a63630",
+    "url": "/portfolio-app/static/js/2.39cbbf94.chunk.js"
+  },
+  {
+    "revision": "d31d38cc9d1353552328",
+    "url": "/portfolio-app/static/js/main.e283f898.chunk.js"
+  },
+  {
+    "revision": "816a5ffaf0da978c157b",
+    "url": "/portfolio-app/static/js/runtime-main.5354bd47.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio-app/static/media/logo.25bf045c.svg"
+  }
+]);
